caa56d1b3fc1cb84ac3c50498c6d11012bf7f027
David Jeusette <jeusette.david@gmail.com>
2019-08-06 21:51:42 +0200

    Bump version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Commander.MixProject do
   def project do
     [
       app: :commander,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
